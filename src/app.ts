1c4a51b2cde27c69b6233cb66c472eaaaaebe694
NagaSoloMuadz <63999419+NagaSolo@users.noreply.github.com>
2020-05-01 04:40:50 +0100

    Create app.ts
    typescript game file

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import "phaser";
+const config: GameConfig = {
+  title: "Lari Lari Anak",
+  width: 800,
+  height: 600,
+  parent: "game"
+  backgroundColor: "#18216D"
+};
+export class LarilarianakGame extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+}
+window.onload = () => {
+  var game = new LarilarianakGame(config);
+};
